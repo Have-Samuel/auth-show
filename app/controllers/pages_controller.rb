d949ea99634fa71df2193b2ac8de0ceba1407eef
Have Samuel <samiehave9@gmail.com>
2026-07-08 07:30:21 +0300

    Controller methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+  allow_unauthenticated_access only: :home
+
+  def new
+  end
+
   def home
+    redirect_to dashboard_path if authenticated?
   end
 
   def dashboard
+    redirect_to new_session_path unless authenticated?
   end
 end
